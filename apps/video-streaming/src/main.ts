e6315c8fbff4978ace7cb5c0caa34d3eaa05f77d
jameskinyua590@gmail.com <jameskinyua590@gmail.com>
2026-01-13 22:04:05 +0300

    log microservice name on boot
    Signed-off-by: jameskinyua590@gmail.com <jameskinyua590@gmail.com>

--- apps/video-streaming/src/main.ts
+++ apps/video-streaming/src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   }
   await app.listen(port);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}`
+    `🚀 video-streaming microservice is running on: http://localhost:${port}`
   );
 }
 
