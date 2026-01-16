46311a6af7673c35de442a6e5027663e4b01595a
jameskinyua590@gmail.com <jameskinyua590@gmail.com>
2026-01-16 10:23:46 +0300

    configure history db connection
    Signed-off-by: jameskinyua590@gmail.com <jameskinyua590@gmail.com>

--- apps/history/src/app/app.module.ts
+++ apps/history/src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+const HISTORY_DBNAME = process.env.HISTORY_DBNAME ?? '';
+const HISTORY_DBHOST = process.env.HISTORY_DBHOST ?? '';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(`${HISTORY_DBHOST}/${HISTORY_DBNAME}`)],
   controllers: [AppController],
   providers: [AppService],
 })
