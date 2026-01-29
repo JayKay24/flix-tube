4e0a5720e1dd4a47768d1544ba3f370128092339
jameskinyua590@gmail.com <jameskinyua590@gmail.com>
2026-01-29 20:08:49 +0300

    split build_images function to build each image separately
    Signed-off-by: jameskinyua590@gmail.com <jameskinyua590@gmail.com>

--- docker-compose-scripts.sh
+++ docker-compose-scripts.sh
@@ -1,18 +1,58 @@
 #!/usr/bin/env bash
 
-build_images() #@ USAGE: build_images PROD
+build_azure_storage_image() #@ USAGE: build_azure_storage_image PROD
 {
-  prod_dev=$1
+  prod_dev_deps=$1
   tag=$2
+  amd=$3
 
-  if [[ $prod_dev = "prod" ]] && [[ -n $tag ]];
+  if [[ $prod_dev_deps = "prod" ]] && [[ -n $tag ]];
   then
-    docker build -f apps/azure-storage/Dockerfile-prod --platform linux/amd64  -t "azure-storage:${tag:-latest}" .
-    docker build -f apps/video-streaming/Dockerfile-prod --platform linux/amd64 -t "video-streaming:${tag:-latest}" .
-    docker build -f apps/history/Dockerfile-prod --platform linux/amd64 -t "history:${tag:-latest}" .
+    if [[ $amd = "amd" ]];
+    then
+      docker build -f apps/azure-storage/Dockerfile-prod --platform linux/amd64 -t "azure-storage:${tag:-latest}" .
+    else
+      docker build -f apps/azure-storage/Dockerfile-prod -t "azure-storage:${tag:-latest}" .
+    fi
   else
     docker build -f apps/azure-storage/Dockerfile-dev -t azure-storage .
+  fi
+}
+
+build_video_streaming_image() #@ USAGE: build_video_streaming_image PROD
+{
+  prod_dev_deps=$1
+  tag=$2
+  amd=$3
+
+  if [[ $prod_dev_deps = "prod" ]] && [[ -n $tag ]];
+  then
+    if [[ $amd = "amd" ]];
+    then
+      docker build -f apps/video-streaming/Dockerfile-prod --platform linux/amd64 -t "video-streaming:${tag:-latest}" .
+    else
+      docker build -f apps/video-streaming/Dockerfile-prod -t "video-streaming:${tag:-latest}" .
+    fi
+  else
     docker build -f apps/video-streaming/Dockerfile-dev -t video-streaming .
+  fi
+}
+
+build_history_image() #@ USAGE: build_history_image PROD
+{
+  prod_dev_deps=$1
+  tag=$2
+  amd=$3
+
+  if [[ $prod_dev_deps = "prod" ]] && [[ -n $tag ]];
+  then
+    if [[ $amd = "amd" ]];
+    then
+      docker build -f apps/history/Dockerfile-prod --platform linux/amd64 -t "history:${tag:-latest}" .
+    else
+      docker build -f apps/history/Dockerfile-prod -t "history:${tag:-latest}" .
+    fi
+  else
     docker build -f apps/history/Dockerfile-dev -t history .
   fi
 }
