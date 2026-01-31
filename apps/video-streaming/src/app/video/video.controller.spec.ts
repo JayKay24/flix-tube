aeb80ced27d7f21f012ccc345e255fe6c965d681
jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>
2026-01-31 12:20:34 +0300

    mock the dependencies for video-controller & video-service
    Signed-off-by: jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>

--- apps/video-streaming/src/app/video/video.controller.spec.ts
+++ apps/video-streaming/src/app/video/video.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { VideoController } from './video.controller';
 import { VideoService } from './video.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Video } from './schemas/video.schema';
+import { ProducerService } from '@flix-tube/rmq-broker';
 
 describe('VideoController', () => {
   let controller: VideoController;
@@ -8,7 +11,21 @@ describe('VideoController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [VideoController],
-      providers: [VideoService],
+      providers: [
+        VideoService,
+        {
+          provide: getModelToken(Video.name),
+          useValue: {
+            findById: jest.fn(),
+          },
+        },
+        {
+          provide: ProducerService,
+          useValue: {
+            sendMessage: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<VideoController>(VideoController);
--- apps/video-streaming/src/app/video/video.service.spec.ts
+++ apps/video-streaming/src/app/video/video.service.spec.ts
@@ -1,12 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { VideoService } from './video.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Video } from './schemas/video.schema';
 
 describe('VideoService', () => {
   let service: VideoService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [VideoService],
+      providers: [
+        VideoService,
+        {
+          provide: getModelToken(Video.name),
+          useValue: {
+            // Add mock methods that are used in the service
+            findById: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<VideoService>(VideoService);
