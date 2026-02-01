4689f6753c09a282b1a48efe371e0ff637f92108
jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>
2026-02-01 11:25:32 +0300

    quote vars in configmaps
    Signed-off-by: jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 deployment_env=$1
-kubectl apply -k $deployment_env --dry-run=client -o yaml | \
+kubectl apply -k $deployment_env --dry-run="client" -o json | \
   envsubst | \
   kubectl apply -f -
