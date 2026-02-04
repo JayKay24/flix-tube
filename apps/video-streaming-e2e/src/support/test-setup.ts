6eb2dc3930a5cdd24a61accc700d7fab419b88af
jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>
2026-02-04 23:46:36 +0300

    reference correct env vars in video-streaming-e2e configs
    Signed-off-by: jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>

--- apps/video-streaming-e2e/src/support/test-setup.ts
+++ apps/video-streaming-e2e/src/support/test-setup.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 module.exports = async function () {
   // Configure axios for tests to use.
-  const host = process.env.HOST ?? 'localhost';
-  const port = process.env.PORT ?? '3000';
+  const host = process.env.TEST_VSTREAM_HOST ?? 'localhost';
+  const port = process.env.TEST_VSTREAM_PORT ?? 4002;
   axios.defaults.baseURL = `http://${host}:${port}`;
 };
