fab6daeebc0b2c13a98a85cbf5360a3ddffd6dbe
jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>
2026-02-08 20:29:35 +0300

    implement routes for db-fixture-rest-api
    Signed-off-by: jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>

--- apps/db-fixture-rest-api/src/app/app.service.ts
+++ apps/db-fixture-rest-api/src/app/app.service.ts
@@ -1,8 +1,64 @@
 import { Injectable } from '@nestjs/common';
+import Fixtures from 'node-mongodb-fixtures';
+import path from 'path';
+import globby from 'globby';
+import * as fs from 'fs';
+
+const fixturesDirectory = process.env.FIXTURES_DIR || 'fixtures';
+const databaseHost = process.env.DB_FIXTURES_HOST || 'mongodb://localhost:27017';
 
 @Injectable()
 export class AppService {
   getData(): { message: string } {
     return { message: 'Hello API' };
+  }
+
+  async loadFixture(databaseName: string, fixtureName: string) {
+    const fixturePath = path.join(fixturesDirectory, fixtureName);
+    let dir = fixturePath;
+    let filter;
+
+    if (fs.existsSync(fixturePath) && fs.statSync(fixturePath).isFile()) {
+      dir = path.dirname(fixturePath);
+      filter = path.basename(fixturePath);
+    }
+
+    const fixtures = new Fixtures({
+      dir,
+      filter,
+      mute: false
+    });
+
+    await fixtures.connect(`${databaseHost}/${databaseName}`);
+    await fixtures.unload();
+    await fixtures.load();
+    await fixtures.disconnect();
+  }
+
+  async unloadFixture(databaseName: string, fixtureName: string) {
+    const fixturePath = path.join(fixturesDirectory, fixtureName);
+    let dir = fixturePath;
+    let filter;
+
+    if (fs.existsSync(fixturePath) && fs.statSync(fixturePath).isFile()) {
+      dir = path.dirname(fixturePath);
+      filter = path.basename(fixturePath);
+    }
+
+    const fixtures = new Fixtures({
+      dir,
+      filter,
+      mute: false
+    });
+
+    await fixtures.connect(`${databaseHost}/${databaseName}`);
+    await fixtures.unload();
+    await fixtures.disconnect();
+  }
+
+  async getFixturesDirs() {
+    const fixtureFilePaths = await globby.globby([fixturesDirectory + "/**/*.js", fixturesDirectory + "/**/*.json"]);
+    console.log("Fixture File Paths: ", fixtureFilePaths);
+    return fixtureFilePaths.map((fixtureFilePath) => path.basename(path.dirname(fixtureFilePath)));
   }
 }
--- libs/dynamic-db/src/lib/dynamic-db.service.ts
+++ libs/dynamic-db/src/lib/dynamic-db.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectConnection } from '@nestjs/mongoose';
-import { Connection, Model, Schema } from 'mongoose';
+import { Collection, Connection, Document, Model, Schema } from 'mongoose';
 
 @Injectable()
 export class FlixTubeDynamicDbService {
@@ -12,5 +12,45 @@ export class FlixTubeDynamicDbService {
       return dynamicDbConnection.model<T>(collectionName, schema);
     }
     return dynamicDbConnection.models[collectionName] as Model<T>;
+  }
+
+  async collectionExists(dbName: string, collectionName: string): Promise<boolean> {
+    const dynamicDbConnection = this.connection.useDb(dbName);
+    try {
+      const collections = await dynamicDbConnection.db?.listCollections({ name: collectionName }).toArray();
+      if (collections) {
+        return collections.length > 0;
+      }
+      return false;
+    } catch (error) {
+      console.error(`Error checking collection existence: ${error}`);
+      return false;
+    }
+  }
+
+  async getCollection(dbName: string, collectionName: string): Promise<Document<any>[] | any[]> {
+    const collectionALreadyExists = await this.collectionExists(dbName, collectionName);
+    if (collectionALreadyExists) {
+      const dynamicDbConnection = this.connection.useDb(dbName);
+      return await dynamicDbConnection.collection(collectionName).find().toArray()
+    } else {
+      console.log("Collection does not exist. Skipping get: " + collectionName);
+      return [];
+    }
+  }
+
+  async dropCollection(dbName: string, collectionName: string): Promise<void> {
+    const collectionALreadyExists = await this.collectionExists(dbName, collectionName);
+    if (collectionALreadyExists) {
+      const dynamicDbConnection = this.connection.useDb(dbName);
+      await dynamicDbConnection.collection(collectionName).drop();
+    } else {
+      console.log("Collection does not exist. Skipping drop: " + collectionName);
+    }
+  }
+
+  async dropDatabase(dbName: string): Promise<void> {
+    const dynamicDbConnection = this.connection.useDb(dbName);
+    await dynamicDbConnection.dropDatabase();
   }
 }
