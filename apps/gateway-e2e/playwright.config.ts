e26f21ecc8bb95756b77620db2178cfc7c323dfe
jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>
2026-02-09 20:48:31 +0300

    override playwright port to use 3000
    Signed-off-by: jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>

--- apps/gateway-e2e/playwright.config.ts
+++ apps/gateway-e2e/playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   },
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npx nx run @flix-tube/gateway:dev',
+    command: 'PORT=3000 npx nx run @flix-tube/gateway:dev',
     url: 'http://localhost:3000',
     reuseExistingServer: true,
     cwd: workspaceRoot,
--- apps/gateway-e2e/src/flixtube-frontend.spec.ts
+++ apps/gateway-e2e/src/flixtube-frontend.spec.ts
@@ -5,7 +5,7 @@ const { describe } = test;
 
 describe("FlixTube Gateway", () => {
   test('can list videos', async ({ page }) => {
-    await loadFixture('metadata', 'two-videos');
+    await loadFixture('metadata', 'videos.json');
 
     await page.goto('/');
 
@@ -14,11 +14,11 @@ describe("FlixTube Gateway", () => {
 
     const firstVideo = videos.nth(0).locator("a");
     await expect(firstVideo).toHaveText("SampleVideo_1280x720_1mb.mp4")
-    await expect(firstVideo).toHaveAttribute("/video?id=5ea234a1c34230004592eb32")
+    await expect(firstVideo).toHaveAttribute("href", "/video?id=5ea234a1c34230004592eb32")
 
     const secondVideo = videos.nth(1).locator("a");
     await expect(secondVideo).toHaveText("Another_video.mp4")
-    await expect(secondVideo).toHaveAttribute("/video?id=5ea234a5c34230004592eb33")
+    await expect(secondVideo).toHaveAttribute("href", "/video?id=5ea234a5c34230004592eb33")
   });
 });
 
