8918b54c428b77727cb7436d258552043fd4a5e2
jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>
2026-02-09 23:25:42 +0300

    configure playwright to only use the started docker-compose gateway application
    Signed-off-by: jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>

--- apps/gateway-e2e/playwright.config.ts
+++ apps/gateway-e2e/playwright.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig, devices } from '@playwright/test';
 import { nxE2EPreset } from '@nx/playwright/preset';
-import { workspaceRoot } from '@nx/devkit';
+// import { workspaceRoot } from '@nx/devkit';
 
 // For CI, you may want to set BASE_URL to the deployed application.
-const baseURL = process.env['BASE_URL'] || 'http://localhost:3000';
+const baseURL = process.env['BASE_URL'] || 'http://localhost:4005';
 
 /**
  * Read environment variables from file.
@@ -23,12 +23,12 @@ export default defineConfig({
     trace: 'on-first-retry',
   },
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'PORT=3000 npx nx run @flix-tube/gateway:dev',
-    url: 'http://localhost:3000',
-    reuseExistingServer: true,
-    cwd: workspaceRoot,
-  },
+  // webServer: {
+  //   command: 'PORT=3000 npx nx run @flix-tube/gateway:dev',
+  //   url: 'http://localhost:3000',
+  //   reuseExistingServer: true,
+  //   cwd: workspaceRoot,
+  // },
   projects: [
     {
       name: 'chromium',
