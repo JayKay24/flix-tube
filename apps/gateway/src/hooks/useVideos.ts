2b6368052948aa32c1055d2fb72cd711bf8e2121
jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>
2026-02-09 15:23:04 +0300

    set up gateway to fetch videos from metadata microservice
    Signed-off-by: jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>

---
+++ apps/gateway/src/hooks/useVideos.ts
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import axios from "axios";
+
+interface IVideo {
+  _id: string;
+  videoPath: string;
+};
+
+function useVideos() {
+  const [videos, setVideos] = useState<IVideo[]>([]);
+
+  async function fetchVideos(urlPath: string) {
+    try {
+      const response = await axios.get<IVideo[]>(urlPath);
+      setVideos(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return { videos, fetchVideos };
+}
+
+export { useVideos };
+export type { IVideo };
