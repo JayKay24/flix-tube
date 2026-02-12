b2a04fa445d939236d3cfe2ffac3bddc243e0db5
jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>
2026-02-12 19:57:18 +0300

    unit test the metadata microservice controller methods findAll, findOne & create
    Signed-off-by: jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>

--- apps/metadata/src/app/video/tests/video.service.spec.ts
+++ apps/metadata/src/app/video/tests/video.service.spec.ts
@@ -4,9 +4,20 @@ import { VideoService } from '../video.service';
 describe('VideoService', () => {
   let service: VideoService;
 
+  const mockVideoService = {
+    findAll: jest.fn(),
+    findOne: jest.fn(),
+    create: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [VideoService],
+      providers: [
+        {
+          provide: VideoService,
+          useValue: mockVideoService,
+        },
+      ],
     }).compile();
 
     service = module.get<VideoService>(VideoService);
