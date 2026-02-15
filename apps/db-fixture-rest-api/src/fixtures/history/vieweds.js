42b2abab0764ccfb7b11f1fdf1283526234f79ce
jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>
2026-02-15 15:38:07 +0300

    update history integration tests
    Signed-off-by: jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>

---
+++ apps/db-fixture-rest-api/src/fixtures/history/vieweds.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+module.exports = [
+  {
+    // _id: new mongoose.Types.ObjectId("5ea234a1c34230004592eb32"),
+    videoId: new mongoose.Types.ObjectId("5ea234a1c34230004592eb32"),
+  }
+];
--- apps/history-e2e/src/history/history.spec.ts
+++ apps/history-e2e/src/history/history.spec.ts
@@ -1,10 +1,27 @@
 import axios from 'axios';
+import { loadFixture } from '@flix-tube/mongodb-fixtures';
 
-describe('GET /api', () => {
+describe('GET /', () => {
   it('should return a message', async () => {
     const res = await axios.get('/');
 
     expect(res.status).toBe(200);
     expect(res.data).toEqual({ message: 'Hello API' });
+  });
+});
+
+describe('GET /viewed', () => {
+  beforeEach(async () => {
+    await loadFixture('history', 'history/vieweds.js');
+  });
+
+  it('should return a message', async () => {
+    const res = await axios.get('/viewed');
+    const { data } = res;
+    const videoId = '5ea234a1c34230004592eb32';
+
+    expect(res.status).toBe(200);
+    expect(data.length).toBe(1);
+    expect(data[0].videoId).toBe(videoId);
   });
 });
