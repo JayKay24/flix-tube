62d11134cdfc5c5a38d1fe50f8e3cc23982ed733
jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>
2026-02-17 12:53:04 +0300

    convert the file into a readable stream when uploading from gateway microservice
    Signed-off-by: jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>

--- apps/gateway/src/app/api/upload/route.ts
+++ apps/gateway/src/app/api/upload/route.ts
@@ -5,10 +5,10 @@ export async function POST(req: Request) {
   const response = await axios({
     method: "POST",
     url: `${process.env.NEXT_PUBLIC_VIDEO_UPLOAD_HOST}/upload`,
-    data: req,
+    data: req.body ? Readable.fromWeb(req.body as any) : null,
     headers: {
-      'Content-Type': req.headers.get('content-type'),
-      'File-Name': req.headers.get('file-name')
+      'Content-Type': req.headers.get('content-type') ?? '',
+      'File-Name': req.headers.get('file-name') ?? ''
     },
     responseType: "stream"
   });
