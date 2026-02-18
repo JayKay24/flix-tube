797e770ae769a39ad814441472ec33d14149859a
jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>
2026-02-18 21:46:35 +0300

    add db-fixture-rest-api to list of images to be pushed
    Signed-off-by: jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>

--- scripts/push_all_images.sh
+++ scripts/push_all_images.sh
@@ -10,6 +10,7 @@ docker_images+=("history")
 docker_images+=("metadata")
 docker_images+=("video-streaming")
 docker_images+=("video-upload")
+docker_images+=("db-fixture-rest-api")
 
 for docker_image in ${docker_images[@]}; do
   docker push $CONTAINER_REGISTRY/$docker_image:$VERSION
