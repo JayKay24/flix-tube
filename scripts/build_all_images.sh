c767927f4a9bb4999f55bca7ab4c19a8877c774b
jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>
2026-02-18 12:19:18 +0300

    implement script to build all images
    Signed-off-by: jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>

---
+++ scripts/build_all_images.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+docker_images=(
+  "azure-storage:apps/azure-storage/Dockerfile-prod"
+  "gateway:apps/gateway/Dockerfile-prod"
+  "history:apps/history/Dockerfile-prod"
+  "metadata:apps/metadata/Dockerfile-prod"
+  "video-streaming:apps/video-streaming/Dockerfile-prod"
+  "video-upload:apps/video-upload/Dockerfile-prod"
+  "db-fixture-rest-api:apps/db-fixture-rest-api/Dockerfile-prod"
+)
+
+for item in "${docker_images[@]}"; do
+  image_name="${item%%:*}"
+  dockerfile="${item#*:}"
+  docker build -t $CONTAINER_REGISTRY/$image_name:$VERSION --file $dockerfile .
+done
--- scripts/push_all_images.sh
+++ scripts/push_all_images.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
+echo $REGISTRY_PW | \
+  docker login $CONTAINER_REGISTRY --username $REGISTRY_UN --password-stdin
+
 docker_images=()
-docker_images+=(azure-storage)
-docker_images+=(gateway)
-docker_images+=(history)
-docker_images+=(metadata)
-docker_images+=(video-streaming)
-docker_images+=(video-upload)
+docker_images+=("azure-storage")
+docker_images+=("gateway")
+docker_images+=("history")
+docker_images+=("metadata")
+docker_images+=("video-streaming")
+docker_images+=("video-upload")
 
 for docker_image in ${docker_images[@]}; do
-  ./push_image.sh $docker_image
+  docker push $CONTAINER_REGISTRY/$docker_image:$VERSION
 done
