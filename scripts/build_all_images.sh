07a309c0cabfbfbca994bd46b93a5633deab0832
jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>
2026-02-19 12:57:34 +0300

    optionally build images for the current platform
    Signed-off-by: jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>

--- scripts/build_all_images.sh
+++ scripts/build_all_images.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+current_platform=$1
+
 declare -A docker_images
 
 docker_images["azure-storage"]="apps/azure-storage/Dockerfile-prod"
@@ -13,5 +15,10 @@ docker_images["db-fixture-rest-api"]="apps/db-fixture-rest-api/Dockerfile-prod"
 for item in "${!docker_images[@]}"; do
   image_name=$item
   dockerfile=${docker_images[${item}]}
-  docker buildx build -t $CONTAINER_REGISTRY/$image_name:$VERSION --file $dockerfile --platform linux/amd64 .
+  if [[ -n $current_platform ]];
+  then
+    docker buildx build -t $CONTAINER_REGISTRY/$image_name:$VERSION --file $dockerfile .
+  else
+    docker buildx build -t $CONTAINER_REGISTRY/$image_name:$VERSION --file $dockerfile --platform linux/amd64 .
+  fi
 done
--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -2,5 +2,11 @@
 
 image_name=$1
 dockerfile=$2
+current_platform=$3
 
-docker buildx build -t $CONTAINER_REGISTRY/$image_name:$VERSION --file $dockerfile --platform linux/amd64 .
+if [[ -n $current_platform ]];
+then
+  docker buildx build -t $CONTAINER_REGISTRY/$image_name:$VERSION --file $dockerfile .
+else
+  docker buildx build -t $CONTAINER_REGISTRY/$image_name:$VERSION --file $dockerfile --platform linux/amd64 .
+fi
