5ff191943be7e724bdba051e4464f8f9566b5ce1
jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>
2026-02-19 11:56:37 +0300

    update syntax to use bash 5+
    Signed-off-by: jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>

--- scripts/build_all_images.sh
+++ scripts/build_all_images.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
-# update this script after upgrading to bash 5
-docker_images=(
-  "azure-storage:apps/azure-storage/Dockerfile-prod"
-  "gateway:apps/gateway/Dockerfile-prod"
-  "history:apps/history/Dockerfile-prod"
-  "metadata:apps/metadata/Dockerfile-prod"
-  "video-streaming:apps/video-streaming/Dockerfile-prod"
-  "video-upload:apps/video-upload/Dockerfile-prod"
-  "db-fixture-rest-api:apps/db-fixture-rest-api/Dockerfile-prod"
-)
+declare -A docker_images
 
-for item in "${docker_images[@]}"; do
-  image_name="${item%%:*}"
-  dockerfile="${item#*:}"
+docker_images["azure-storage"]="apps/azure-storage/Dockerfile-prod"
+docker_images["gateway"]="apps/gateway/Dockerfile-prod"
+docker_images["history"]="apps/history/Dockerfile-prod"
+docker_images["metadata"]="apps/metadata/Dockerfile-prod"
+docker_images["video-streaming"]="apps/video-streaming/Dockerfile-prod"
+docker_images["video-upload"]="apps/video-upload/Dockerfile-prod"
+docker_images["db-fixture-rest-api"]="apps/db-fixture-rest-api/Dockerfile-prod"
+
+for item in "${!docker_images[@]}"; do
+  image_name=$item
+  dockerfile=${docker_images[${item}]}
   docker buildx build -t $CONTAINER_REGISTRY/$image_name:$VERSION --file $dockerfile --platform linux/amd64 .
 done
