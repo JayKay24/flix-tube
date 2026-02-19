d251f8fb18582f6bf30c921adbce8a264177656b
jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>
2026-02-19 22:33:04 +0300

    pass in the cluster environment when executing scripts to build images
    Signed-off-by: jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>

--- scripts/build_all_images.sh
+++ scripts/build_all_images.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 current_platform=$1
+cluster_env=$2
 
 declare -A docker_images
 
@@ -15,10 +16,10 @@ docker_images["db-fixture-rest-api"]="apps/db-fixture-rest-api/Dockerfile-prod"
 for item in "${!docker_images[@]}"; do
   image_name=$item
   dockerfile=${docker_images[${item}]}
-  if [[ -n $current_platform ]];
+  if [[ $current_platform == "linux/amd64" ]];
   then
-    docker buildx build -t $CONTAINER_REGISTRY/$image_name:$VERSION --file $dockerfile .
+    docker buildx build -t $CONTAINER_REGISTRY/$cluster_env-$image_name:$VERSION --file $dockerfile --platform linux/amd64 .
   else
-    docker buildx build -t $CONTAINER_REGISTRY/$image_name:$VERSION --file $dockerfile --platform linux/amd64 .
+    docker buildx build -t $CONTAINER_REGISTRY/$cluster_env-$image_name:$VERSION --file $dockerfile .
   fi
 done
--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -3,10 +3,11 @@
 image_name=$1
 dockerfile=$2
 current_platform=$3
+cluster_env=$4
 
-if [[ -n $current_platform ]];
+if [[ $current_platform == "linux/amd64" ]];
 then
-  docker buildx build -t $CONTAINER_REGISTRY/$image_name:$VERSION --file $dockerfile .
+  docker buildx build -t $CONTAINER_REGISTRY/$cluster_env-image_name:$VERSION --file $dockerfile --platform linux/amd64 .
 else
-  docker buildx build -t $CONTAINER_REGISTRY/$image_name:$VERSION --file $dockerfile --platform linux/amd64 .
+  docker buildx build -t $CONTAINER_REGISTRY/$cluster_env-image_name:$VERSION --file $dockerfile .
 fi
