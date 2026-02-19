a5eed1bad13de58a9e95ecd64c46baccef561825
jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>
2026-02-19 15:19:04 +0300

    rename web_app to production
    Signed-off-by: jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>

--- infra/terraform/production/main.tf
+++ infra/terraform/production/main.tf
@@ -6,7 +6,7 @@ module "common" {
 }
 
 module "production" {
-  source = "../modules/web_app"
+  source = "../modules/production"
   flixtube_az_region_name = var.flixtube_az_region_name
   flixtube_az_subscription_id = var.flixtube_az_subscription_id
   flixtube_az_resource_group_name = var.flixtube_az_resource_group_name
