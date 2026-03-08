f58f6c101e32b6dfa3ab6471be758b32b33626fb
jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>
2026-03-08 14:46:10 +0300

    feat: Implement readiness and liveness probes for the history service, integrating RabbitMQ and MongoDB health checks and updating the K8s deployment.
    Signed-off-by: jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>

--- apps/history/src/app/app.service.ts
+++ apps/history/src/app/app.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
+import { ProducerService } from '@flix-tube/rmq-broker';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @InjectConnection() private connection: Connection,
+    private readonly producerService: ProducerService,
+  ) {}
+
   getData(): { message: string } {
     return { message: 'Hello API' };
+  }
+
+  async checkReadiness(): Promise<boolean> {
+    const isMongoReady = this.connection.readyState === 1;
+    const isRabbitReady = await this.producerService.isReady();
+
+    return isMongoReady && isRabbitReady;
   }
 }
