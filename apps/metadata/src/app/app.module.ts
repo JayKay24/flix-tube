b0c5cdd5c0d759c02711df537c755bec13db35b1
jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>
2026-03-08 14:52:56 +0300

    feat: Implement readiness and liveness probes and relocate the RMQ broker module to the root application module.
    Signed-off-by: jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>

--- apps/metadata/src/app/app.module.ts
+++ apps/metadata/src/app/app.module.ts
@@ -3,12 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VideoModule } from './video/video.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { FlixTubeRmqBrokerModule } from '@flix-tube/rmq-broker';
 
 const METADATA_DBHOST = process.env.METADATA_DBHOST ?? '';
 const METADATA_DBNAME = process.env.METADATA_DBNAME ?? '';
 
 @Module({
-  imports: [MongooseModule.forRoot(`${METADATA_DBHOST}/${METADATA_DBNAME}`), VideoModule],
+  imports: [
+    MongooseModule.forRoot(`${METADATA_DBHOST}/${METADATA_DBNAME}`), 
+    VideoModule, 
+    FlixTubeRmqBrokerModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
