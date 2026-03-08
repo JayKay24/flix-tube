d9c46b066e8fe55cf7dc89ca32f554085e7d16ac
jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>
2026-03-08 16:40:51 +0300

    test: mock ProducerService in app controller and service unit tests.
    Signed-off-by: jameskinyua590@gmail.com <20414083+JayKay24@users.noreply.github.com>

--- apps/video-streaming/src/app/tests/app.controller.spec.ts
+++ apps/video-streaming/src/app/tests/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from '../app.controller';
 import { AppService } from '../app.service';
+import { ProducerService } from "@flix-tube/rmq-broker";
 
 describe('AppController', () => {
   let app: TestingModule;
@@ -8,7 +9,15 @@ describe('AppController', () => {
   beforeAll(async () => {
     app = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService, {
+          provide: ProducerService,
+          useValue: {
+            sendMessage: jest.fn(),
+            isReady: jest.fn().mockResolvedValue(true),
+          },
+        }
+      ],
     }).compile();
   });
 
